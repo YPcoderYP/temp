a2c03de5b5f912d2f55291e15b5a6f0b6de52ddf
YashParsana <yashparsana135@gamil.com>
2021-12-17 15:16:29 +0530

    /path

--- index.js
+++ index.js
@@ -18,7 +18,54 @@ app.use(express.static(path.join(__dirname, "/public")));
 // });
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
+
+    // res.render("weather");
+    // let city='surat';
+    // requests("http://api.openweathermap.org/data/2.5/weather?q=surat&appid=a3ca8441e71fb696fcec5e7d4ef1f366")
+    //     .on("data", (defaultdata) => { data = [JSON.parse(defaultdata)];});
+    
+    fun=(city="surat")=> {
+        requests(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3ca8441e71fb696fcec5e7d4ef1f366`)
+        .on("data", (apidata) => {
+                data = JSON.parse(apidata);
+                data = [data];
+                // console.log(data);
+                return res.render("weather", {
+                    tempval: (data[0].main.temp - 273.15).toFixed(2),
+                    speed: data[0].wind.speed,
+                    Humidity: data[0].main.humidity,
+                    location: data[0].name,
+                    country: data[0].sys.country, 
+                });
+        });
+    };
+
+    app.post("/userinput", urlencodedParser, (req, res) => {
+        
+        let city = req.body.city;
+        
+        requests(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3ca8441e71fb696fcec5e7d4ef1f366`)
+        .on("data", (apidata) => {
+                data = JSON.parse(apidata);
+                data = [data];
+                // console.log(data);
+                res.render("weather", {
+                    tempval: (data[0].main.temp - 273.15).toFixed(2),
+                    speed: data[0].wind.speed,
+                    Humidity: data[0].main.humidity,
+                    location: data[0].name,
+                    country: data[0].sys.country, 
+                });
+        }).on("error", (err) => {
+            console.log("city not found");
+        });
+    });
+    fun();
+
+});
+
+app.get("/*", (req, res) => {
 
     // res.render("weather");
     // let city='surat';
